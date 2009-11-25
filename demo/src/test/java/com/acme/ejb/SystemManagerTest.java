f51150fae238ff53f16d6f5cd2e09ae5c6a132f7
Aslak Knutsen <aslak@redhat.com>
2009-11-25 13:53:45 +0000

    ARQ-17 Changed to use org.junit.Assert

--- demo/src/test/java/com/acme/ejb/SystemManagerTest.java
+++ demo/src/test/java/com/acme/ejb/SystemManagerTest.java
@@ -18,12 +18,12 @@ package com.acme.ejb;
 
 import javax.ejb.EJB;
 
-import junit.framework.Assert;
 
 import org.jboss.arquillian.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.Archives;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
