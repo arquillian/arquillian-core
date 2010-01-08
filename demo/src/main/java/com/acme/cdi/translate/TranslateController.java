002ed0e1097942223035940722c16ab470f2e902
Aslak Knutsen <aslak@redhat.com>
2010-01-08 14:36:44 +0000

    ARQ-35 ARQ-54 Added more examples for the Weld SE container, taken from the Weld Reference guide

---
+++ demo/src/main/java/com/acme/cdi/translate/TranslateController.java
@@ -0,0 +1,54 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2009, Red Hat Middleware LLC, and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.acme.cdi.translate;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+@Named
+@SessionScoped
+public class TranslateController implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Inject
+	TextTranslator textTranslator;
+
+	private String inputText;
+
+	private String translation;
+
+	// JSF action method, perhaps
+	public void translate() {
+		translation = textTranslator.translate(inputText);
+	}
+
+	public String getInputText() {
+		return inputText;
+	}
+
+	public void setInputText(String text) {
+		this.inputText = text;
+	}
+
+	public String getTranslation() {
+		return translation;
+	}
+}
---
+++ demo/src/test/java/com/acme/cdi/payment/SynchronousPaymentProcessorTestCase.java
@@ -0,0 +1,58 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2009, Red Hat Middleware LLC, and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.acme.cdi.payment;
+
+import javax.inject.Inject;
+
+import org.jboss.arquillian.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archives;
+import org.jboss.shrinkwrap.api.Paths;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * InjectionTestCase
+ *
+ * @author <a href="mailto:aslak@conduct.no">Aslak Knutsen</a>
+ * @version $Revision: $
+ */
+@RunWith(Arquillian.class)
+public class SynchronousPaymentProcessorTestCase {
+
+	@Deployment
+	public static JavaArchive createDeployment() {
+		return Archives.create("test.jar", JavaArchive.class)
+				.addPackage(
+						Synchronous.class.getPackage()
+				)
+				.addManifestResource(
+						"com/acme/cdi/payment/beans.xml",
+						Paths.create("beans.xml"));
+	}
+	
+	@Inject @Synchronous PaymentProcessor processor;
+	
+	@Test
+	public void shouldBeReplacedByAMock() throws Exception
+	{
+		processor.process("");
+		Assert.assertTrue(MockPaymentProcessor.HAS_BEEN_CALLED);
+	}
+}
