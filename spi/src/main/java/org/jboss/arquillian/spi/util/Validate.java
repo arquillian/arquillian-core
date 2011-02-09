9038dae3d8a37658a465bea572fc0a24723a1205
Karel Piwko <kpiwko@redhat.com>
2011-02-09 10:40:50 +0100

    ARQ-168 fixed directory check

--- spi/src/main/java/org/jboss/arquillian/spi/util/Validate.java
+++ spi/src/main/java/org/jboss/arquillian/spi/util/Validate.java
@@ -88,9 +88,10 @@ public final class Validate
     */
    public static void configurationDirectoryExists(final String string, final String message) throws ConfigurationException
    {
-      if (string == null || string.length() == 0 || new File(string).isDirectory())
+      if (string == null || string.length() == 0 || new File(string).isDirectory() == false)
       {
          throw new ConfigurationException(message);
       }
-   }
+   }  
+
 }
