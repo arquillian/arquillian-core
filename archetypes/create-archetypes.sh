6ed5a316d83439c63c59eaadf70c8bffc0ffee9c
Aslak Knutsen <aslak@redhat.com>
2011-04-27 21:55:18 +0200

    ARQ-361 Remove old api/spi/impl-base/archetypes

--- archetypes/create-archetypes.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-mvn -f junit-javaee6/pom.xml clean archetype:create-from-project -Darchetype.properties=../junit-javaee6-archetype.properties
-mvn -f junit-javaee6/target/generated-sources/archetype/pom.xml install
