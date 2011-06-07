bb0714c99b0db726c6387cef97d4a515a58d987d
Aslak Knutsen <aslak@redhat.com>
2011-06-07 15:39:09 +0200

    AS-734 Add toString to TestResult.

--- test/spi/src/main/java/org/jboss/arquillian/test/spi/TestResult.java
+++ test/spi/src/main/java/org/jboss/arquillian/test/spi/TestResult.java
@@ -26,144 +26,169 @@ import java.io.Serializable;
  * @author <a href="mailto:aknutsen@redhat.com">Aslak Knutsen</a>
  * 
  */
-public final class TestResult implements Serializable {
-	private static final long serialVersionUID = 1L;
+public final class TestResult implements Serializable
+{
+   private static final long serialVersionUID = 1L;
 
-	/**
-	 * The test status
-	 * 
-	 * @author Pete Muir
-	 * 
-	 */
-	public enum Status {
-		/**
-		 * The test passed
-		 */
-		PASSED,
-		/**
-		 * The test failed
-		 */
-		FAILED,
-		/**
-		 * The test was skipped due to some deployment problem
-		 */
-		SKIPPED;
-	}
+   /**
+    * The test status
+    * 
+    * @author Pete Muir
+    * 
+    */
+   public enum Status {
+      /**
+       * The test passed
+       */
+      PASSED,
+      /**
+       * The test failed
+       */
+      FAILED,
+      /**
+       * The test was skipped due to some deployment problem
+       */
+      SKIPPED;
+   }
 
-	private Status status;
-	transient private Throwable throwable;
-	private ExceptionProxy exceptionProxy;
+   private Status status;
 
-	private long start;
-	private long end;
+   transient private Throwable throwable;
 
-	/**
-	 * Create a empty result.<br/>
-	 * <br/>
-	 * Start time is set to Current Milliseconds.
-	 */
-	public TestResult() {
-		this(null);
-	}
+   private ExceptionProxy exceptionProxy;
 
-	/**
-	 * Create a new TestResult.<br/>
-	 * <br/>
-	 * Start time is set to Current Milliseconds.
-	 * 
-	 * @param status
-	 *            The result status.
-	 */
-	public TestResult(Status status) {
-		this(status, null);
-	}
+   private long start;
 
-	/**
-	 * Create a new TestResult.<br/>
-	 * <br/>
-	 * Start time is set to Current Milliseconds.
-	 * 
-	 * @param status
-	 *            The result status.
-	 * @param throwable
-	 *            thrown exception if any
-	 */
-	public TestResult(Status status, Throwable throwable) {
-		this.status = status;		
-		setThrowable(throwable);
+   private long end;
 
-		this.start = System.currentTimeMillis();
-	}
+   /**
+    * Create a empty result.<br/>
+    * <br/>
+    * Start time is set to Current Milliseconds.
+    */
+   public TestResult()
+   {
+      this(null);
+   }
 
-	/**
-	 * Get the status of this test
-	 */
-	public Status getStatus() {
-		return status;
-	}
+   /**
+    * Create a new TestResult.<br/>
+    * <br/>
+    * Start time is set to Current Milliseconds.
+    * 
+    * @param status
+    *            The result status.
+    */
+   public TestResult(Status status)
+   {
+      this(status, null);
+   }
 
-	public void setStatus(Status status) {
-		this.status = status;
-	}
+   /**
+    * Create a new TestResult.<br/>
+    * <br/>
+    * Start time is set to Current Milliseconds.
+    * 
+    * @param status
+    *            The result status.
+    * @param throwable
+    *            thrown exception if any
+    */
+   public TestResult(Status status, Throwable throwable)
+   {
+      this.status = status;
+      setThrowable(throwable);
 
-	/**
-	 * If the test failed, the exception that was thrown. It does not need to be
-	 * the root cause.
-	 */
-	public Throwable getThrowable() {
-		if (throwable == null) {
-			if (exceptionProxy != null) {
-				throwable = exceptionProxy.createException();	
-			}			
-		}
-		return throwable;
-	}
+      this.start = System.currentTimeMillis();
+   }
 
-	public void setThrowable(Throwable throwable) {
-		this.throwable = throwable;		
-		this.exceptionProxy = ExceptionProxy.createForException(throwable);
-	}
+   /**
+    * Get the status of this test
+    */
+   public Status getStatus()
+   {
+      return status;
+   }
 
-	/**
-	 * Set the start time of the test.
-	 * 
-	 * @param start
-	 *            Start time in milliseconds
-	 */
-	public void setStart(long start) {
-		this.start = start;
-	}
+   public void setStatus(Status status)
+   {
+      this.status = status;
+   }
 
-	/**
-	 * Get the start time.
-	 * 
-	 * @return Start time in milliseconds
-	 */
-	public long getStart() {
-		return start;
-	}
+   /**
+    * If the test failed, the exception that was thrown. It does not need to be
+    * the root cause.
+    */
+   public Throwable getThrowable()
+   {
+      if (throwable == null)
+      {
+         if (exceptionProxy != null)
+         {
+            throwable = exceptionProxy.createException();
+         }
+      }
+      return throwable;
+   }
 
-	/**
-	 * Set the end time of the test.
-	 * 
-	 * @param End
-	 *            time in milliseconds
-	 */
-	public void setEnd(long end) {
-		this.end = end;
-	}
+   public void setThrowable(Throwable throwable)
+   {
+      this.throwable = throwable;
+      this.exceptionProxy = ExceptionProxy.createForException(throwable);
+   }
 
-	/**
-	 * Get the end time.
-	 * 
-	 * @return End time in milliseconds
-	 */
-	public long getEnd() {
-		return end;
-	}
+   /**
+    * Set the start time of the test.
+    * 
+    * @param start
+    *            Start time in milliseconds
+    */
+   public void setStart(long start)
+   {
+      this.start = start;
+   }
 
-	public ExceptionProxy getExceptionProxy() {
-		
-		return exceptionProxy;
-	}	
+   /**
+    * Get the start time.
+    * 
+    * @return Start time in milliseconds
+    */
+   public long getStart()
+   {
+      return start;
+   }
+
+   /**
+    * Set the end time of the test.
+    * 
+    * @param End
+    *            time in milliseconds
+    */
+   public void setEnd(long end)
+   {
+      this.end = end;
+   }
+
+   /**
+    * Get the end time.
+    * 
+    * @return End time in milliseconds
+    */
+   public long getEnd()
+   {
+      return end;
+   }
+
+   public ExceptionProxy getExceptionProxy()
+   {
+
+      return exceptionProxy;
+   }
+
+   @Override
+   public String toString()
+   {
+      long time = (end > 0 ? end - start : System.currentTimeMillis() - start);
+      return "TestResult[status=" + status + ",time=" + time + "ms]";
+   }
 }
