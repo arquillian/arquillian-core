081a6240f86598ed9011df873486630f802eae49
Aslak Knutsen <aslak@redhat.com>
2013-08-27 11:58:01 +0200

    Set BeanManager to null during Shutdown
    * Possible memory leak

--- testenrichers/cdi/src/main/java/org/jboss/arquillian/testenricher/cdi/container/CDIExtension.java
+++ testenrichers/cdi/src/main/java/org/jboss/arquillian/testenricher/cdi/container/CDIExtension.java
@@ -3,25 +3,30 @@ package org.jboss.arquillian.testenricher.cdi.container;
 import javax.enterprise.event.Observes;
 import javax.enterprise.inject.spi.BeanManager;
 import javax.enterprise.inject.spi.BeforeBeanDiscovery;
+import javax.enterprise.inject.spi.BeforeShutdown;
 import javax.enterprise.inject.spi.Extension;
 
 public class CDIExtension implements Extension
 {
-	private static BeanManager beanManager;
+   private static BeanManager beanManager;
 
-	void beforeBeanDiscovery(@Observes BeforeBeanDiscovery beforeBeanDiscovery,
-			BeanManager beanManager)
-	{
-		setBeanManager(beanManager);
-    }
+   void beforeBeanDiscovery(@Observes BeforeBeanDiscovery beforeBeanDiscovery, BeanManager beanManager)
+   {
+      setBeanManager(beanManager);
+   }
 
-	public static BeanManager getBeanManager()
-	{
-		return beanManager;
-	}
+   void beforeShutdown(@Observes BeforeShutdown beforeShutdown)
+   {
+      setBeanManager(null);
+   }
 
-	private static void setBeanManager(BeanManager beanManager)
-	{
-		CDIExtension.beanManager = beanManager;
-	}
+   public static BeanManager getBeanManager()
+   {
+      return beanManager;
+   }
+
+   private static void setBeanManager(BeanManager beanManager)
+   {
+      CDIExtension.beanManager = beanManager;
+   }
 }
