4f08053e679c2e7ac482d5375cc036d33d3ee6ab
Aslak Knutsen <aslak@4fs.no>
2015-03-11 11:47:15 +0100

    ARQ-197 WIP New Arquillian Runner delegate should implement Filterable/Sortable

--- junit/core/src/main/java/org/jboss/arquillian/junit/Arquillian.java
+++ junit/core/src/main/java/org/jboss/arquillian/junit/Arquillian.java
@@ -2,15 +2,34 @@ package org.jboss.arquillian.junit;
 
 import org.junit.runner.Description;
 import org.junit.runner.Runner;
+import org.junit.runner.manipulation.Filter;
+import org.junit.runner.manipulation.Filterable;
+import org.junit.runner.manipulation.NoTestsRemainException;
+import org.junit.runner.manipulation.Sortable;
+import org.junit.runner.manipulation.Sorter;
 import org.junit.runner.notification.RunNotifier;
 import org.junit.runners.model.InitializationError;
 
-public class Arquillian extends Runner {
+public class Arquillian extends Runner implements Filterable, Sortable {
 
     private Runner delegate;
     
     public Arquillian(Class<?> testClass) throws InitializationError {
         this.delegate = Runners.runners(testClass);
+    }
+
+    @Override
+    public void sort(Sorter sorter) {
+       if(delegate instanceof Sortable) {
+          ((Sortable)delegate).sort(sorter);
+       }
+    }
+
+    @Override
+    public void filter(Filter filter) throws NoTestsRemainException {
+       if(delegate instanceof Filterable) {
+          ((Filterable)delegate).filter(filter);
+       }
     }
 
     @Override
