ad1d4a6487c14324cccc78fd40329bd891a384ed
Dipak Pawar <dpawar@redhat.com>
2017-12-20 13:37:27 +0530

    Changed copyright year from 2009 to 2017

--- container/test-api/src/main/java/org/jboss/arquillian/container/test/api/Config.java
+++ container/test-api/src/main/java/org/jboss/arquillian/container/test/api/Config.java
@@ -1,6 +1,6 @@
 /*
  * JBoss, Home of Professional Open Source.
- * Copyright 2009 Red Hat Inc. and/or its affiliates and other contributors
+ * Copyright 2017 Red Hat Inc. and/or its affiliates and other contributors
  * as indicated by the @author tags. See the copyright.txt file in the
  * distribution for a full listing of individual contributors.
  *
