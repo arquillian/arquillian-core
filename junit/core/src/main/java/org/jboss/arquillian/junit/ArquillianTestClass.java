fd25fef0165aa9d5b711cf083dc25200c1abb347
Matous Jobanek <mjobanek@redhat.com>
2018-01-04 13:00:55 +0100

    chore: merged the same code to avoid code duplication & added missing rules enrichment in Arquillian Rule implementation

--- junit/core/src/main/java/org/jboss/arquillian/junit/ArquillianTestClass.java
+++ junit/core/src/main/java/org/jboss/arquillian/junit/ArquillianTestClass.java
@@ -1,8 +1,6 @@
 package org.jboss.arquillian.junit;
 
-import org.jboss.arquillian.test.spi.LifecycleMethodExecutor;
 import org.jboss.arquillian.test.spi.TestRunnerAdaptor;
-import org.jboss.arquillian.test.spi.TestRunnerAdaptorBuilder;
 import org.junit.rules.TestRule;
 import org.junit.runner.Description;
 import org.junit.runners.model.Statement;
@@ -28,57 +26,21 @@ public class ArquillianTestClass implements TestRule {
         return new Statement() {
             @Override
             public void evaluate() throws Throwable {
-                State.runnerStarted();
-                // first time we're being initialized
-                if (!State.hasTestAdaptor()) {
-                    // no, initialization has been attempted before and failed, refuse
-                    // to do anything else
-                    if (State.hasInitializationException()) {
-                        // failed on suite level, ignore children
-                        // notifier.fireTestIgnored(getDescription());
-                        throw new RuntimeException(
-                            "Arquillian initialization has already been attempted, but failed. See previous exceptions for cause",
-                                State.getInitializationException());
-                    } else {
-                        try {
-                            // ARQ-1742 If exceptions happen during boot
-                            TestRunnerAdaptor adaptor = TestRunnerAdaptorBuilder
-                                    .build();
-                            // don't set it if beforeSuite fails
-                            adaptor.beforeSuite();
-                            State.testAdaptor(adaptor);
-                        } catch (Exception e) {
-                            // caught exception during BeforeSuite, mark this as failed
-                            State.caughtInitializationException(e);
-                            State.runnerFinished();
-                            if (State.isLastRunner()) {
-                                State.clean();
-                            }
-                            throw e;
+                ArquillianTestClassLifecycleManager arquillian =
+                    new ArquillianTestClassLifecycleManager() {
+                        protected void setAdaptor(TestRunnerAdaptor testRunnerAdaptor) {
+                            adaptor = testRunnerAdaptor;
                         }
-                    }
-                }
 
-                // initialization ok, run children
-                if (State.hasTestAdaptor()) {
-                    adaptor = State.getTestAdaptor();
-                }
-
-                adaptor.beforeClass(description.getTestClass(), LifecycleMethodExecutor.NO_OP);
+                        protected TestRunnerAdaptor getAdaptor() {
+                            return adaptor;
+                        }
+                    };
+                arquillian.beforeTestClassPhase(description.getTestClass());
                 try {
                     base.evaluate();
                 } finally {
-                    adaptor.afterClass(description.getTestClass(), LifecycleMethodExecutor.NO_OP);
-                    State.runnerFinished();
-                    if (State.isLastRunner()) {
-                        try {
-                            adaptor.afterSuite();
-                            adaptor.shutdown();
-                        } finally {
-                            State.clean();
-                        }
-                    }
-                    adaptor = null;
+                    arquillian.afterTestClassPhase(description.getTestClass());
                 }
             }
         };
