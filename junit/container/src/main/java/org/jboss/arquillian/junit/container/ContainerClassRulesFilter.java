3526c4c8e6874e335c090cd3a8c8099f25c51984
Bartosz Majsak <bartosz.majsak@gmail.com>
2018-01-23 10:21:50 +0100

    chore: updates javadoc for class rules filter

--- junit/container/src/main/java/org/jboss/arquillian/junit/container/ContainerClassRulesFilter.java
+++ junit/container/src/main/java/org/jboss/arquillian/junit/container/ContainerClassRulesFilter.java
@@ -8,7 +8,8 @@ import org.junit.rules.TestRule;
 public class ContainerClassRulesFilter implements JUnitClassRulesFilter {
 
     /**
-     * No @ClassRule should be executed inside of a container since the state between @Test is not kept there.
+     * No @ClassRule should be executed inside of a container since the state between @Test is not kept there. 
+     * This means that particular @ClassRule can be executed multiple times per one test class executed in the container.
      */
     public List<TestRule> filter(List<TestRule> scannedRules) {
         return new ArrayList<TestRule>();
