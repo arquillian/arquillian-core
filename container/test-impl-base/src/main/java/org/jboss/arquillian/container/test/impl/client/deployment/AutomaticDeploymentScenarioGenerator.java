b4e2aab0d9c805819568b9d33d9e5cd413988a0c
Alex Soto <asotobu@gmail.com>
2018-01-27 22:37:39 +0100

    fix: Adds javadoc

--- container/test-impl-base/src/main/java/org/jboss/arquillian/container/test/impl/client/deployment/AutomaticDeploymentScenarioGenerator.java
+++ container/test-impl-base/src/main/java/org/jboss/arquillian/container/test/impl/client/deployment/AutomaticDeploymentScenarioGenerator.java
@@ -1,3 +1,19 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2010 Red Hat Inc. and/or its affiliates and other contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.jboss.arquillian.container.test.impl.client.deployment;
 
 import java.lang.reflect.Method;
@@ -9,6 +25,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.logging.Logger;
 import org.jboss.arquillian.container.spi.client.deployment.DeploymentDescription;
+import org.jboss.arquillian.container.spi.client.deployment.DeploymentScenario;
 import org.jboss.arquillian.container.spi.client.deployment.TargetDescription;
 import org.jboss.arquillian.container.spi.client.deployment.Validate;
 import org.jboss.arquillian.container.spi.client.protocol.ProtocolDescription;
@@ -22,6 +39,12 @@ import org.jboss.arquillian.container.test.spi.util.ServiceLoader;
 import org.jboss.arquillian.test.spi.TestClass;
 import org.jboss.shrinkwrap.api.Archive;
 
+/**
+ * {@link DeploymentScenarioGenerator} that builds a {@link DeploymentScenario} based on
+ * the {@link AutomaticDeployment} registered services.
+ *
+ * @version $Revision: $
+ */
 public class AutomaticDeploymentScenarioGenerator implements DeploymentScenarioGenerator {
 
     private static Logger log = Logger.getLogger(AutomaticDeploymentScenarioGenerator.class.getName());
