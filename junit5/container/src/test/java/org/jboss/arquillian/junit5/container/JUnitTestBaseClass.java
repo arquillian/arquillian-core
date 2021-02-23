08ca39685bd8a85413a12ec36aa529a8d7a0cb05
Johannes Beck <mail@johannes-beck.name>
2021-02-23 10:37:36 +0100

    Update junit5/container/src/test/java/org/jboss/arquillian/junit5/container/JUnitTestBaseClass.java
    Co-authored-by: Bartosz Majsak <bartosz.majsak@gmail.com>

--- junit5/container/src/test/java/org/jboss/arquillian/junit5/container/JUnitTestBaseClass.java
+++ junit5/container/src/test/java/org/jboss/arquillian/junit5/container/JUnitTestBaseClass.java
@@ -1,6 +1,6 @@
 /*
  * JBoss, Home of Professional Open Source
- * Copyright 2011 Red Hat Inc. and/or its affiliates and other contributors
+ * Copyright 2021 Red Hat Inc. and/or its affiliates and other contributors
  * as indicated by the @authors tag. All rights reserved.
  * See the copyright.txt in the distribution for a
  * full listing of individual contributors.
