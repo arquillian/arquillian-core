f298a4b792d7aa180f90600180522ad1ae93bf86
lprimak <lenny@flowlogix.com>
2021-03-22 14:32:13 -0500

    removed hardcoded constant

--- testng/core/src/main/java/org/jboss/arquillian/testng/Arquillian.java
+++ testng/core/src/main/java/org/jboss/arquillian/testng/Arquillian.java
@@ -223,7 +223,7 @@ public abstract class Arquillian implements IHookable {
                 testResult.setThrowable(result.getThrowable());
 
                 // setting status as failed.
-                testResult.setStatus(2);
+                testResult.setStatus(ITestResult.FAILURE);
             }
 
             // calculate test end time. this is overwritten in the testng invoker..
