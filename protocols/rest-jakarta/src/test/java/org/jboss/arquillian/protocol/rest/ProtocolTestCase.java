8bb0a63816ee8399ea4db17c9e094378a789c420
Jared Anderson <jhanders@us.ibm.com>
2022-07-26 21:41:31 -0500

    Add a REST protocol option to not depend on servlet to run tests
    - Create a RESTful Web Services arquillian protocol so do not need to
    depend on servlet in an application server in order to run tests on the
    server.
    - Duplicate the servlet-jakarta tests in the rest protocol project and
    update them to test the REST protocol

---
+++ protocols/rest-jakarta/src/test/java/org/jboss/arquillian/protocol/rest/ProtocolTestCase.java
@@ -0,0 +1,144 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2009, 2022 Red Hat Inc. and/or its affiliates and other contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.arquillian.protocol.rest;
+
+import java.net.URL;
+
+import org.jboss.arquillian.protocol.rest.test.MockTestRunner;
+import org.jboss.arquillian.protocol.rest.test.TestCommandCallback;
+import org.jboss.arquillian.protocol.servlet5.ServletProtocolConfiguration;
+import org.jboss.arquillian.protocol.rest.runner.RESTTestRunner;
+import org.jboss.arquillian.test.spi.TestResult;
+import org.jboss.arquillian.test.spi.TestResult.Status;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * ProtocolTestCase
+ *
+ * @author <a href="mailto:aslak@conduct.no">Aslak Knutsen</a>
+ * @version $Revision: $
+ */
+public class ProtocolTestCase extends AbstractServerBase {
+
+    @Test
+    public void shouldReturnTestResult() throws Exception {
+        MockTestRunner.add(TestResult.passed());
+
+        RESTMethodExecutor executor = createExecutor();
+        TestResult result = executor.invoke(new MockTestExecutor());
+
+        Assert.assertEquals(
+            "Should have returned a passed test",
+            MockTestRunner.wantedResults.getStatus(),
+            result.getStatus());
+
+        Assert.assertNull(
+            "No Exception should have been thrown",
+            result.getThrowable());
+    }
+
+    @Test
+    public void shouldUseLogicalMethodNameAndSupportUrlReservedCharactersInIt() throws Exception {
+        final String testMethodNameWithReservedUrlCharacters = "non standard!test&name";
+
+        MockTestRunner.add(TestResult.passed());
+
+        RESTMethodExecutor executor = createExecutor();
+        executor.invoke(new MockTestExecutor() {
+            @Override
+            public String getMethodName() {
+                return testMethodNameWithReservedUrlCharacters;
+            }
+        });
+
+        Assert.assertEquals(
+            "Should use the logical method name",
+            testMethodNameWithReservedUrlCharacters,
+            MockTestRunner.testRequests.get(0).getMethodName()
+        );
+    }
+
+    @Test
+    public void shouldReturnThrownException() throws Exception {
+        MockTestRunner.add(TestResult.failed(new Exception().fillInStackTrace()));
+
+        RESTMethodExecutor executor = createExecutor();
+        TestResult result = executor.invoke(new MockTestExecutor());
+
+        Assert.assertEquals(
+            "Should have returned a passed test",
+            MockTestRunner.wantedResults.getStatus(),
+            result.getStatus());
+
+        Assert.assertNotNull(
+            "Exception should have been thrown",
+            result.getThrowable());
+    }
+
+    @Test
+    public void shouldReturnExceptionWhenMissingTestClassParameter() throws Exception {
+        URL url = createURL(RESTTestRunner.OUTPUT_MODE_SERIALIZED, null, null);
+        TestResult result = (TestResult) TestUtil.execute(url);
+
+        Assert.assertEquals(
+            "Should have returned a passed test",
+            Status.FAILED,
+            result.getStatus());
+
+        Assert.assertTrue(
+            "No Exception should have been thrown",
+            result.getThrowable() instanceof IllegalArgumentException);
+    }
+
+    @Test
+    public void shouldReturnExceptionWhenMissingMethodParameter() throws Exception {
+        URL url = createURL(RESTTestRunner.OUTPUT_MODE_SERIALIZED, "org.my.test", null);
+        TestResult result = (TestResult) TestUtil.execute(url);
+
+        Assert.assertEquals(
+            "Should have returned a passed test",
+            Status.FAILED,
+            result.getStatus());
+
+        Assert.assertTrue(
+            "No Exception should have been thrown",
+            result.getThrowable() instanceof IllegalArgumentException);
+    }
+
+    @Test
+    public void shouldReturnExceptionWhenErrorLoadingClass() throws Exception {
+        URL url = createURL(RESTTestRunner.OUTPUT_MODE_SERIALIZED, "org.my.test", "test");
+        TestResult result = (TestResult) TestUtil.execute(url);
+
+        Assert.assertEquals(
+            "Should have returned a passed test",
+            Status.FAILED,
+            result.getStatus());
+
+        Assert.assertTrue(
+            "No Exception should have been thrown",
+            result.getThrowable() instanceof ClassNotFoundException);
+    }
+
+    protected RESTMethodExecutor createExecutor() {
+        return new RESTMethodExecutor(
+            new ServletProtocolConfiguration(),
+            createContexts(),
+            new TestCommandCallback());
+    }
+}
---
+++ protocols/rest-jakarta/src/test/java/org/jboss/arquillian/protocol/rest/RESTDeploymentPackagerTestCase.java
@@ -0,0 +1,299 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2009, 2022 Red Hat Inc. and/or its affiliates and other contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.arquillian.protocol.rest;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import org.jboss.arquillian.container.spi.client.deployment.Validate;
+import org.jboss.arquillian.container.test.api.Testable;
+import org.jboss.arquillian.container.test.spi.TestDeployment;
+import org.jboss.arquillian.container.test.spi.client.deployment.ProtocolArchiveProcessor;
+import org.jboss.arquillian.protocol.servlet5.arq514hack.descriptors.api.application.ApplicationDescriptor;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ArchivePaths;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.Asset;
+import org.jboss.shrinkwrap.api.asset.StringAsset;
+import org.jboss.shrinkwrap.api.spec.EnterpriseArchive;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.jboss.shrinkwrap.api.spec.ResourceAdapterArchive;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.descriptor.api.Descriptors;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * RESTDeploymentPackagerTestCase
+ *
+ * @author <a href="mailto:aslak@redhat.com">Aslak Knutsen</a>
+ * @version $Revision: $
+ */
+public class RESTDeploymentPackagerTestCase {
+    @Test
+    public void shouldHandleJavaArchive() throws Exception {
+        Archive<?> archive = new RESTDeploymentPackager().generateDeployment(
+            new TestDeployment(null,
+                ShrinkWrap.create(JavaArchive.class, "applicationArchive.jar"),
+                createAuxiliaryArchives()),
+            processors());
+
+        Assert.assertTrue(
+            "Verify that a defined JavaArchive using EE6 JavaArchive protocol is build as WebArchive",
+            Validate.isArchiveOfType(WebArchive.class, archive));
+
+        Assert.assertTrue(
+            "Verify that the auxiliaryArchives are placed in /WEB-INF/lib",
+            archive.contains(ArchivePaths.create("/WEB-INF/lib/arquillian-jakarta-rest-protocol.jar")));
+
+        Assert.assertTrue(
+            "Verify that the auxiliaryArchives are placed in /WEB-INF/lib",
+            archive.contains(ArchivePaths.create("/WEB-INF/lib/auxiliaryArchive2.jar")));
+
+        Assert.assertTrue(
+            "Verify that the applicationArchive is placed in /WEB-INF/lib",
+            archive.contains(ArchivePaths.create("/WEB-INF/lib/applicationArchive.jar")));
+
+        Assert.assertTrue(
+            "Verify protocol Processor SPI was called",
+            DummyProcessor.wasCalled);
+    }
+
+    @Test
+    public void shouldHandleWebArchive() throws Exception {
+        Archive<?> archive = new RESTDeploymentPackager().generateDeployment(
+            new TestDeployment(null,
+                ShrinkWrap.create(WebArchive.class, "applicationArchive.war"),
+                createAuxiliaryArchives()),
+            processors());
+
+        Assert.assertTrue(
+            "Verify that a defined WebArchive using EE9 JavaArchive protocol is build as WebArchive",
+            Validate.isArchiveOfType(WebArchive.class, archive));
+
+        Assert.assertTrue(
+            "Verify that the auxiliaryArchives are placed in /WEB-INF/lib",
+            archive.contains(ArchivePaths.create("/WEB-INF/lib/arquillian-jakarta-rest-protocol.jar")));
+
+        Assert.assertTrue(
+            "Verify that the auxiliaryArchives are placed in /WEB-INF/lib",
+            archive.contains(ArchivePaths.create("/WEB-INF/lib/auxiliaryArchive1.jar")));
+
+        Assert.assertTrue(
+            "Verify that the auxiliaryArchives are placed in /WEB-INF/lib",
+            archive.contains(ArchivePaths.create("/WEB-INF/lib/auxiliaryArchive2.jar")));
+
+        Assert.assertTrue(
+            "Verify protocol Processor SPI was called",
+            DummyProcessor.wasCalled);
+    }
+
+    @Test
+    public void shouldHandleEnterpriseArchive() throws Exception {
+        Archive<?> archive = new RESTDeploymentPackager().generateDeployment(
+            new TestDeployment(null,
+                ShrinkWrap.create(EnterpriseArchive.class, "applicationArchive.ear"),
+                createAuxiliaryArchives()),
+            processors());
+
+        Assert.assertTrue(
+            "Verify that the auxiliaryArchives are placed in /",
+            archive.contains(ArchivePaths.create(JarNameProcessor.jarName)));
+
+        Assert.assertTrue(
+            "Verify that the auxiliaryArchives are placed in /lib",
+            archive.contains(ArchivePaths.create("/lib/auxiliaryArchive1.jar")));
+
+        Assert.assertTrue(
+            "Verify that the auxiliaryArchives are placed in /lib",
+            archive.contains(ArchivePaths.create("/lib/auxiliaryArchive2.jar")));
+
+        Assert.assertTrue(
+            "Verify protocol Processor SPI was called",
+            DummyProcessor.wasCalled);
+    }
+
+    @Test
+    public void shouldHandleEnterpriseArchiveWithApplicationXML() throws Exception {
+        Archive<?> archive = new RESTDeploymentPackager().generateDeployment(
+            new TestDeployment(null,
+                ShrinkWrap.create(EnterpriseArchive.class, "applicationArchive.ear")
+                    .setApplicationXML(createApplicationDescriptor()),
+                createAuxiliaryArchives()),
+            processors());
+
+        Assert.assertTrue(
+            "Verify that the auxiliaryArchives are placed in /",
+            archive.contains(ArchivePaths.create(JarNameProcessor.jarName)));
+
+        Assert.assertTrue(
+            "Verify that the auxiliaryArchives are placed in /lib",
+            archive.contains(ArchivePaths.create("/lib/auxiliaryArchive1.jar")));
+
+        Assert.assertTrue(
+            "Verify that the auxiliaryArchives are placed in /lib",
+            archive.contains(ArchivePaths.create("/lib/auxiliaryArchive2.jar")));
+
+        String applicationXmlContent =
+            TestUtil.convertToString(archive.get("META-INF/application.xml").getAsset().openStream());
+        Assert.assertTrue(
+            "verify that the arquillian-jakarta-rest-protocol.war was added to the application.xml",
+            applicationXmlContent.contains(String.format("<web-uri>%s</web-uri>", JarNameProcessor.jarName)));
+
+        // ARQ-670
+        Assert.assertTrue(
+            "verify that the arquillian-jakarta-rest-protocol.war has correct context-root in application.xml",
+            applicationXmlContent.contains(String.format("<context-root>%s</context-root>",
+                    JarNameProcessor.jarName.replaceFirst(".war$", ""))));
+
+        Assert.assertTrue(
+            "Verify protocol Processor SPI was called",
+            DummyProcessor.wasCalled);
+    }
+
+    @Test
+    public void shouldHandleEnterpriseArchiveWithWebArchive() throws Exception {
+        WebArchive applicationWar = ShrinkWrap.create(WebArchive.class, "applicationArchive.war");
+
+        Archive<?> archive = new RESTDeploymentPackager().generateDeployment(
+            new TestDeployment(null,
+                ShrinkWrap.create(EnterpriseArchive.class, "applicationArchive.ear")
+                    .addAsModule(applicationWar),
+                createAuxiliaryArchives()),
+            processors());
+
+        Assert.assertFalse(
+            "Verify that the auxiliaryArchives was not added",
+            archive.contains(ArchivePaths.create("arquillian-jakarta-rest-protocol.war")));
+
+        Assert.assertTrue(
+            "Verify that the auxiliaryArchives are placed in /lib",
+            archive.contains(ArchivePaths.create("/lib/auxiliaryArchive1.jar")));
+
+        Assert.assertTrue(
+            "Verify that the auxiliaryArchives are placed in /lib",
+            archive.contains(ArchivePaths.create("/lib/auxiliaryArchive2.jar")));
+
+        Assert.assertTrue(
+            "Verify protocol Processor SPI was called",
+            DummyProcessor.wasCalled);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldThrowExceptionOnUnknownArchiveType() throws Exception {
+        new RESTDeploymentPackager().generateDeployment(
+            new TestDeployment(null, ShrinkWrap.create(ResourceAdapterArchive.class), new ArrayList<Archive<?>>()),
+            processors()
+        );
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void shouldThrowExceptionOnEnterpriseArchiveWithMultipleWebArchive() throws Exception {
+        new RESTDeploymentPackager().generateDeployment(
+            new TestDeployment(null,
+                ShrinkWrap.create(EnterpriseArchive.class, "applicationArchive.ear")
+                    .addAsModule(ShrinkWrap.create(WebArchive.class))
+                    .addAsModule(ShrinkWrap.create(WebArchive.class)),
+                createAuxiliaryArchives()),
+            processors());
+    }
+
+    @Test
+    public void shouldHandleEnterpriseArchiveWithMultipleWebArchiveAndOneMarkedWebArchive() throws Exception {
+        WebArchive testableArchive = Testable.archiveToTest(ShrinkWrap.create(WebArchive.class));
+
+        Archive<?> archive = new RESTDeploymentPackager().generateDeployment(
+            new TestDeployment(null,
+                ShrinkWrap.create(EnterpriseArchive.class, "applicationArchive.ear")
+                    .addAsModule(testableArchive)
+                    .addAsModule(ShrinkWrap.create(WebArchive.class)),
+                createAuxiliaryArchives()),
+            processors());
+
+        Assert.assertFalse(
+            "Verify that the auxiliaryArchives was not added",
+            archive.contains(ArchivePaths.create("arquillian-jakarta-rest-protocol.war")));
+
+        Assert.assertTrue(
+            "Verify that the auxiliaryArchives are placed in /lib",
+            archive.contains(ArchivePaths.create("/lib/auxiliaryArchive1.jar")));
+
+        Assert.assertTrue(
+            "Verify that the auxiliaryArchives are placed in /lib",
+            archive.contains(ArchivePaths.create("/lib/auxiliaryArchive2.jar")));
+
+        Assert.assertTrue(
+            "Verify protocol Processor SPI was called",
+            DummyProcessor.wasCalled);
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void shouldThrowExceptionOnEnterpriseArchiveWithMultipleMarkedWebArchives() throws Exception {
+        new RESTDeploymentPackager().generateDeployment(
+            new TestDeployment(null,
+                ShrinkWrap.create(EnterpriseArchive.class, "applicationArchive.ear")
+                    .addAsModule(Testable.archiveToTest(ShrinkWrap.create(WebArchive.class)))
+                    .addAsModule(Testable.archiveToTest(ShrinkWrap.create(WebArchive.class))),
+                createAuxiliaryArchives()),
+            processors());
+    }
+
+    private Collection<Archive<?>> createAuxiliaryArchives() {
+        List<Archive<?>> archives = new ArrayList<Archive<?>>();
+        archives.add(ShrinkWrap.create(JavaArchive.class, "auxiliaryArchive1.jar"));
+        archives.add(ShrinkWrap.create(JavaArchive.class, "auxiliaryArchive2.jar"));
+
+        return archives;
+    }
+
+    private Asset createApplicationDescriptor() {
+        return new StringAsset(
+            Descriptors.create(ApplicationDescriptor.class)
+                .version("6")
+                .ejbModule("test.jar")
+                .exportAsString());
+    }
+
+    private Collection<ProtocolArchiveProcessor> processors() {
+        List<ProtocolArchiveProcessor> pros = new ArrayList<ProtocolArchiveProcessor>();
+        pros.add(new DummyProcessor());
+        pros.add(new JarNameProcessor());
+        return pros;
+    }
+
+    private static class DummyProcessor implements ProtocolArchiveProcessor {
+        public static boolean wasCalled = false;
+
+        public DummyProcessor() {
+            wasCalled = false;
+        }
+
+        @Override
+        public void process(TestDeployment testDeployment, Archive<?> protocolArchive) {
+            wasCalled = true;
+        }
+    }
+
+    private static class JarNameProcessor implements ProtocolArchiveProcessor {
+        static String jarName;
+
+        @Override
+        public void process(TestDeployment testDeployment, Archive<?> protocolArchive) {
+            jarName = protocolArchive.getName();
+        }
+    }
+}
