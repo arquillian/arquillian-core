397923e27c8eba0070d55aeed9fb5cd971a0c324
Scott M Stark <starksm64@gmail.com>
2025-06-21 19:25:59 -0600

    - Expand the PR by @jamezp to remove use of the ContextStore for the TestResult exception and just return TestResult from interceptInvocation. - Add tests of a junit5 suite
    Fixes #725
    
    Signed-off-by: Scott M Stark <starksm@starkinternational.com>

--- junit5/core/src/main/java/org/jboss/arquillian/junit5/ContextStore.java
+++ junit5/core/src/main/java/org/jboss/arquillian/junit5/ContextStore.java
@@ -1,8 +1,6 @@
 package org.jboss.arquillian.junit5;
 
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.Optional;
 
 import org.junit.jupiter.api.extension.ExtensionContext;
 
@@ -10,8 +8,6 @@ class ContextStore {
     private static final String NAMESPACE_KEY = "arquillianNamespace";
 
     private static final String INTERCEPTED_TEMPLATE_NAMESPACE_KEY = "interceptedTestTemplates";
-
-    private static final String RESULT_NAMESPACE_KEY = "results";
 
     private static final String PARAMETER_NAMESPACE_KEY = "methodParameters";
 
@@ -33,10 +29,6 @@ class ContextStore {
         return context.getStore(ExtensionContext.Namespace.create(NAMESPACE_KEY, INTERCEPTED_TEMPLATE_NAMESPACE_KEY));
     }
 
-    private ExtensionContext.Store getResultStore() {
-        return context.getStore(ExtensionContext.Namespace.create(NAMESPACE_KEY, RESULT_NAMESPACE_KEY));
-    }
-
     boolean isRegisteredTemplate(Method method) {
         final ExtensionContext.Store templateStore = getTemplateStore();
 
@@ -45,22 +37,6 @@ class ContextStore {
             templateStore.put(method.toGenericString(), true);
         }
         return isRegistered;
-    }
-
-    void storeResult(String uniqueId, Throwable throwable) {
-        final ExtensionContext.Store resultStore = getResultStore();
-        resultStore.put(uniqueId, throwable);
-        // TODO: find source and unwrap it where it is thrown, not here.
-        if (throwable instanceof InvocationTargetException) {
-            resultStore.put(uniqueId, throwable.getCause());
-        } else {
-            resultStore.put(uniqueId, throwable);
-        }
-    }
-
-    Optional<Throwable> getResult(String uniqueId) {
-        final ExtensionContext.Store resultStore = getResultStore();
-        return Optional.ofNullable(resultStore.getOrDefault(uniqueId, Throwable.class, null));
     }
 
     /**
